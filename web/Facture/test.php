7bfcf093f26afb45b2217053d08b7368c2c779a4
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-04-16 01:04:15 +0200

    tentative

--- web/Facture/test.php
+++ web/Facture/test.php
@@ -1,37 +1,38 @@
 <?php
+
 require('fpdf.php');
+include ('config.inc.php');
 
-class PDF extends tFPDF
+class pdf extends tFPDF
 {
-// En-tête
-function Header()
-{
-    
-    // Police Arial gras 15
-    $this->SetFont('Arial','B',15);
-    // Décalage à droite
-    $this->Cell(80);
-    // Titre
-    $this->Cell(30,10,'Facture',1,0,'C');
-    // Saut de ligne
-    $this->Ln(20);
-}
+    // En-tête
+    function Header()
+    {
+        
+        // Police Arial gras 15
+        $this->SetFont('Arial','B',15);
+        // Décalage à droite
+        $this->Cell(80);
+        // Titre
+        $this->Cell(30,10,'Facture',1,0,'C');
+        // Saut de ligne
+        $this->Ln(20);
+    }
 
-// Pied de page
-function Footer()
-{
-    // Positionnement à 1,5 cm du bas
-    $this->SetY(-15);
-    // Police Arial italique 8
-    $this->SetFont('Arial','I',8);
-    // Numéro de page
-    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    // Pied de page
+    function Footer()
+    {
+        // Positionnement à 1,5 cm du bas
+        $this->SetY(-15);
+        // Police Arial italique 8
+        $this->SetFont('Arial','I',8);
+        // Numéro de page
+        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    }
 }
-}
-$sql="SELECT create_by
-FROM mrbs_entry
-WHERE id = 1";
-$user = sql_query1($sql);
+$sql = "SELECT * FROM mrbs_entry WHERE id=1";
+$req 
+
 // Instanciation de la classe dérivée
 $pdf = new PDF();
 $pdf->AliasNbPages();
