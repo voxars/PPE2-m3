29ad487d7d612b19f3b55e95ece1e3870ed4e5dd
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-12 15:38:25 +0200

    +

--- web/suite/oui.php
+++ web/suite/oui.php
@@ -1,8 +1,11 @@
 <?php
 include 'top.php';
 
-$req = $db->querry('SELECT * FROM mrbs_users WHERE id=1');
-var_dump($req);
+$req = $db->prepare('SELECT * FROM mrbs_users WHERE id=1');
+$req->execute();
+$result = $req->fetch(PDO::FETCH_ASSOC);
+print_r($result);
+
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   
@@ -40,17 +43,13 @@ var_dump($req);
 
 <form>
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="exampleInputEmail1">Pseudo</label>
+    <input type="text" class="form-control" id="pseudo" >
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
+  
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
