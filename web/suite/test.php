8b2c43f6b231c8fb8c1b1ad51196baf1f73216c3
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-12 22:07:29 +0200

    +

--- web/suite/test.php
+++ web/suite/test.php
@@ -1,9 +1,7 @@
 <?php
 
 require('fpdf.php');
-require_once "../defaultincludes.inc";
-require_once "../mrbs_sql.inc";
-require_once "../functions_view.inc";
+include ("connexion.php");
 
 
 class pdf extends tFPDF
@@ -42,9 +40,9 @@ class pdf extends tFPDF
 }
 $id=$_SESSION['id'];
 //var_dump($id);
-$sql = "SELECT from_unixtime(start_time) as debut,from_unixtime(end_time) as  fin,timestampdiff(SECOND,from_unixtime(start_time),from_unixtime(end_time))/3600 as duree, price, create_by,room_name, email FROM mrbs_entry e INNER JOIN mrbs_room r on r.id=e.room_id INNER JOIN mrbs_users u on e.create_by=u.name where e.id = ".$id.";";
-  $info = sql_query($sql);
-  $ligne = sql_row($info,0);
+$sql =$pdo->prepare("SELECT from_unixtime(start_time) as debut,from_unixtime(end_time) as  fin,timestampdiff(SECOND,from_unixtime(start_time),from_unixtime(end_time))/3600 as duree, price, create_by,room_name, email FROM mrbs_entry e INNER JOIN mrbs_room r on r.id=e.room_id INNER JOIN mrbs_users u on e.create_by=u.name where e.id =?;");
+  $info = $sql->execute(array($id));
+  
 
 /* var_dump($info);
 var_dump($ligne); */
@@ -53,15 +51,20 @@ $pdf = new pdf();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
-$pdf->Cell(0,10,'Membre : '.$ligne[4],0,1);
-$pdf->Cell(0,10,'Mail : '.$ligne[6],0,1);
-$pdf->Cell(0,10,'Date : '.$ligne[0],0,1);
-$pdf->Cell(0,10,'Duree : '.$ligne[2],0,1);
-$pdf->Cell(0,10,'Prix total : '.$ligne[2]*$ligne[3],0,1); 
+$pdf->Cell(0,10,'Membre : '.$info['create_by']);
+$pdf->Cell(0,10,'Mail : '.$info['email']);
+$pdf->Cell(0,10,'Date : '.$info['debut']);
+$pdf->Cell(0,10,'Duree : '.$info['duree']);
+$pdf->Cell(0,10,'Prix total : '.$info['price']*$info['duree']); 
 $pdf->Output();
 
  // observations
  $pdf->SetFont( "Arial", "BU", 10 ); $pdf->SetXY( 5, 75 ) ; $pdf->Cell($pdf->GetStringWidth("Observations"), 0, "Observations", 0, "L");
  $pdf->SetFont( "Arial", "", 10 ); $pdf->SetXY( 5, 78 ) ; $pdf->MultiCell(190, 4, $row[5], 0, "L");
 ?>
-
+<form action="session.php" method="POST">
+   <input type="submit" name="bouton" class="btn btn-primary" value="Retour acceuil">
+</form></br>
+<body onLoad="document.fo.login.focus()">
+      [ <a href="deconnexion.php">Se déconnecter</a> ]
+</body>
