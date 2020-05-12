1200136f85f52d09ed0128aa0332316781b21774
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-12 21:35:38 +0200

    +

--- web/suite/all-entry.php
+++ web/suite/all-entry.php
@@ -12,35 +12,75 @@ $idu=$_SESSION['id'];
 $sel=$pdo->prepare("SELECT level FROM mrbs_users WHERE id=?");
    $sel->execute(array($idu));
    $donnees = $sel->fetch();
+   $level = $donnees['level'];
+   
+if($level <= 1)
+{
+    ?>
+    <table class="table">
+    <thead>
+    <tr>
+    <th scope="col"> id </th>
+    <th scope="col"> utilisateur </th>
+    <th scope="col"> description </th>
+    <th scope="col"> date </th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+        $sel=$pdo->prepare("SELECT * FROM mrbs_entry WHERE create_by=?");
+        $sel->execute(array($log));
+        
+        while ($donnees = $sel->fetch())
+        {
+            //On affiche l'id et le nom du client en cours
+            echo "</TR>";
+            echo "<TH scope='row'> ". $donnees['id'] ."</TH>";
+            echo "<TH> ". $donnees['create_by'] ."</TH>";
+            echo "<TH> ". $donnees['description'] ."</TH>";
+            echo "<TH>". $donnees['timestamp'] ."</TH>";
+            echo "</TR>";
+        }
+        
+    ?>
+    </tbody>
+    </table>
+    <?php
+}
+else
+{
+    ?>
+    <table class="table">
+    <thead>
+    <tr>
+    <th scope="col"> id </th>
+    <th scope="col"> utilisateur </th>
+    <th scope="col"> description </th>
+    <th scope="col"> date </th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+        $sel=$pdo->prepare("SELECT * FROM mrbs_entry");
+        $sel->execute(array($log));
+        
+        while ($donnees = $sel->fetch())
+        {
+            //On affiche l'id et le nom du client en cours
+            echo "</TR>";
+            echo "<TH scope='row'> ". $donnees['id'] ."</TH>";
+            echo "<TH> ". $donnees['create_by'] ."</TH>";
+            echo "<TH> ". $donnees['description'] ."</TH>";
+            echo "<TH>". $donnees['timestamp'] ."</TH>";
+            echo "</TR>";
+        }
+        
+    ?>
+    </tbody>
+    </table>
+    <?php
+}
 ?>
-<table class="table">
-<thead>
-<tr>
- <th scope="col"> id </th>
- <th scope="col"> utilisateur </th>
- <th scope="col"> description </th>
- <th scope="col"> date </th>
-  </tr>
-  </thead>
-  <tbody>
-  <?php
-    $sel=$pdo->prepare("SELECT * FROM mrbs_entry WHERE create_by=?");
-    $sel->execute(array($log));
-    
-    while ($donnees = $sel->fetch())
-    {
-        //On affiche l'id et le nom du client en cours
-        echo "</TR>";
-        echo "<TH scope='row'> ". $donnees['id'] ."</TH>";
-        echo "<TH> ". $donnees['create_by'] ."</TH>";
-        echo "<TH> ". $donnees['description'] ."</TH>";
-        echo "<TH>". $donnees['timestamp'] ."</TH>";
-        echo "</TR>";
-    }
-    
-?>
-</tbody>
-</table>
 <form action="session.php" method="POST">
    <input type="submit" name="bouton" class="btn btn-primary" value="Retour acceuil">
 </form></br>
