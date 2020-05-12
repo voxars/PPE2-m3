281167c945f10a96741f9bcfa716fa2bf1d20eeb
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-12 16:12:16 +0200

    yeahhh

--- web/suite/login.php
+++ web/suite/login.php
@@ -18,49 +18,19 @@ include 'top.php';
          $erreur="Mauvais login ou mot de passe!";
    }
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Facture</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    
-  </div>
-</nav>
 
 
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Pseudo</label>
-    <input type="text" class="form-control" id="pseudo" >
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<body onLoad="document.fo.login.focus()">
+   <form name="fo" method="post" action="">
+   <div class="form-group">
+      <label for="exampleInputEmail1">Pseudo</label>
+      <input type="text" class="form-control" name="login" id="pseudo" >
+   </div>
+   <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" name="pass" id="exampleInputPassword1">
+   </div>
+   
+   <button type="submit" name="valider" class="btn btn-primary" values="S'authentifier">Submit</button>
+   </form>
+</body>
