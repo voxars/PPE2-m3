6f242493523ffee686aa30e4fd158e2db31b5df5
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-12 18:11:28 +0200

    +

--- web/suite/session.php
+++ web/suite/session.php
@@ -3,21 +3,7 @@
    if($_SESSION["autoriser"]!="oui"){
       header("location:login.php");
       exit();
-   }
-   @$login=$_POST["login"];
-include('top.php');  
-include('connexion.php');  
-$req=$pdo->query("SELECT * FROM mrbs_entry WHERE creat_by=$login");
-
-while ($re = $req->fetch())
-{
-   echo $re['id'];
-   echo $re['create_by'];
-   echo $re['name'];
-   echo $re['room_id'];
-}
-	
-      
+   }   
 ?>
 
 <button type="submit" name="valider" class="btn btn-primary " values="S'authentifier">Facture</button></br>
