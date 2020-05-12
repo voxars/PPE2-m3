0b38128a306b49cc4c331e4629149990ab255917
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-12 21:12:09 +0200

    +

--- web/suite/all-entry.php
+++ web/suite/all-entry.php
@@ -8,18 +8,31 @@
     }  
 
 $log=$_SESSION['login'];
-
+?>
+<table>
+<tr>
+ <th> id </th>
+ <th> utilisateur </th>
+ <th> description </th>
+ <th> date </th>
+  </tr>
+  <?php
     $sel=$pdo->prepare("SELECT * FROM mrbs_entry WHERE create_by=?");
     $sel->execute(array($log));
+    
     while ($donnees = $sel->fetch())
     {
         //On affiche l'id et le nom du client en cours
-        echo $donnees['id'];
-        echo $donnees['timestamp'];
-        echo $donnees['name'];
-        echo $donnees['description'];
+        echo "</TR>";
+        echo "<TH> ". $donnees['id'] ."</TH>";
+        echo "<TH> ". $donnees['create_by'] ."</TH>";
+        echo "<TH> ". $donnees['description'] ."</TH>";
+        echo "<TH>". $donnees['timestamp'] ."</TH>";
+        echo "</TR>";
     }
+    
 ?>
+</table>
 <form action="session.php" method="POST">
    <input type="submit" name="bouton" class="btn btn-primary" value="Retour acceuil">
 </form></br>
