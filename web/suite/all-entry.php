8ec3d981f55ff0401e47ea9611edaafe2f78bb32
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-13 18:44:52 +0200

    +

--- web/suite/all-entry.php
+++ web/suite/all-entry.php
@@ -26,7 +26,6 @@ if($level <= 1)
     <th scope="col"> description </th>
     <th scope="col"> date </th>
     <th scope="col"> PDF </th>
-    <th scope="col"> Supprimer </th>
     </tr>
     </thead>
     <tbody>
@@ -42,7 +41,6 @@ if($level <= 1)
             echo "<TH> ". $donnees['description'] ."</TH>";
             echo "<TH>". $donnees['timestamp'] ."</TH>";
             echo "<TH>" ?><input type="submit" name="bouton" onclick=window.location.href='http://localhost/M2L-SysResSalles/PPE2-m3/web/suite/facture.php'; class="btn btn-primary" value="Afficher facture"> <form method=POST action='http://localhost/M2L-SysResSalles/PPE2-m3/web/suite/facture.php'><input type="hidden" name="idr" value="<?php echo "".$donnees['id']."" ?>"></input></form><?php "/TH>";
-            echo "<TH>" ?><input type="submit" name="bouton" onclick=window.location.href='http://localhost/M2L-SysResSalles/PPE2-m3/web/suite/facture.php'; class="btn btn-primary" value="Afficher facture"> <?php "/<TH>";
             echo "</TR>";
         }
     ?>
