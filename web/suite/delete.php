a7910e43ca5761543e0bc9feae8c9057bc0e6534
CPerrichon <62330243+CPerrichon@users.noreply.github.com>
2020-05-13 20:03:54 +0200

    j

--- web/suite/delete.php
+++ web/suite/delete.php
@@ -1,5 +1,6 @@
 <?php
-echo $donnees['id'];
+$id=$_POST['idr'];
+echo $id;
 /*$sel=$pdo->prepare("DELETE FROM mrbs_entry WHERE id=?");
 $sel->execute(array($donnees['id']));
 echo "Facture Clôturée";*/
