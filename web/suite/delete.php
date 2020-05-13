b2910e72311cf542bee26ea2766a2b8c3c939229
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2020-05-13 20:09:16 +0200

    +

--- web/suite/delete.php
+++ web/suite/delete.php
@@ -11,7 +11,10 @@ echo $id;
 /*$sel=$pdo->prepare("DELETE FROM mrbs_entry WHERE id=?");
 $sel->execute(array($donnees['id']));
 echo "Facture Clôturée";*/
-?>
+?></br>
+<form action="session.php" method="POST">
+   <input type="submit" name="bouton" class="btn btn-primary" value="Retour acceuil">
+</form></br>
 <body onLoad="document.fo.login.focus()">
       [ <a href="deconnexion.php">Se déconnecter</a> ]
 </body>
